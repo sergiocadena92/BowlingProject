32be2194264ed6436829cbb2b5a0da704fff285d
Sergio Cadena <scadena@easysol.net>
2019-09-15 22:01:04 -0500

    First version

---
+++ src/main/java/com/bowling/demo/BowlingApplication.java
@@ -0,0 +1,13 @@
+package com.bowling.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingApplication.class, args);
+	}
+
+}
